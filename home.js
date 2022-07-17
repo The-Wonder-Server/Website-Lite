0d8c0284ad59b41bd4681e7bb2fd2571f922dc02
Ivar van den Bosch <78146502+IvarvandenBosch@users.noreply.github.com>
2022-07-17 22:18:22 +0200

    Header responsiveness issue fix [📱]
    This adds a toggle for the CSS function to fix the responsiveness issue by removing the padding that fixed a previous issue.

--- home.js
+++ home.js
@@ -1,9 +1,11 @@
 let toggleButton = document.getElementsByClassName('toggle-button')[0]
 let navbarLinks = document.getElementsByClassName('navbar-links')[0]
+let headerBg = document.getElementsByClassName('bg1')[0]
 
 function dropDown() {
     navbarLinks.classList.toggle('open')
     toggleButton.classList.toggle('open')
+    headerBg.classList.toggle('padding-null')
 }
 
 function rulesRedirect() {
